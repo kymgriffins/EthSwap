8e9167baa74fe39d38381c830f0ee5e8f8a84a1f
Griffins Kimutai <49910242+kymgriffins@users.noreply.github.com>
2022-02-08 14:33:04 +0300

    Add Frontend code

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,31 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    );
-  }
+import Web3 from 'web3';
+
+function App() {
+  const [account , setAccount ] = useState('');
+  const [balance , setBalance ] = useState(0);
+  console.log("account", account);
+  console.log(balance)
+  useEffect(() => {
+    async function load(){
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+      console.log(accounts)
+      //Get Balance EthSwap
+      const ethBalance = await web3.eth.getBalance(accounts[0]);
+      setBalance(ethBalance)
+      console.log(ethBalance)
+    }
+    load();
+  },[])
+  
+  return (
+    <div>
+      <h1>Hello {account}</h1>
+    </div>
+  );
 }
 
 export default App;
